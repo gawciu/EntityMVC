a12a47db018f14c529989815932a83bfa399d540
Rafał Gawroński <gawciiu@gmail.com>
2022-01-17 21:13:45 +0100

    Added controller & view

---
+++ EntityMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EntityMVC.Models;
+
+namespace EntityMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var entities = new EmployeesEntities();
+            return View(entities.employees.ToList());
+        }
+    }
+}
